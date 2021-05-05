012119eb9c91c1183f1f89b641097dfa2baf6d2e
Kurniandha Sukma Yunastrian <13516106@std.stei.itb.ac.id>
2021-05-05 20:29:34 +0700

    Add requirements migration table

---
+++ database/migrations/2021_05_05_131925_create_requirements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequirementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requirements', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idProject');
+            $table->integer('number');
+            $table->string('name');
+            $table->double('score', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requirements');
+    }
+}
