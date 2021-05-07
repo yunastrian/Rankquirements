1e63b24a56a5ad18864915da8a4cc45c6341bb01
Kurniandha Sukma Yunastrian <13516106@std.stei.itb.ac.id>
2021-05-07 22:10:32 +0700

    Fix styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,13 +42,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link font-weight-bold text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link font-weight-bold text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
